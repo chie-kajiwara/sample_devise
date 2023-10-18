bb03676b3c9eea56b2b1bade355c471e332bcfc9
chie.kajiwara <chie.kajiwara@ops-in.com>
2023-10-18 09:08:17 +0900

    修正

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -3,6 +3,7 @@ class PasswordsController < Devise::PasswordsController
     super
   rescue ActiveRecord::RecordInvalid => e
     self.resource = e.record
+    self.resource.reset_password_token = resource_params[:reset_password_token]
     respond_with self.resource
   end
 end
